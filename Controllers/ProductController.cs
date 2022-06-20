f2e9ac8e4fff0f300dd0878a94370436a3d28feb
ManaliPavan <jadhav.manali@outlook.com>
2022-06-20 17:22:24 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMVCApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult ProductList()
+        {
+            string[] products = { "Clothes", "Furniture", "Makeup", "Toys" };
+            ViewBag.Products = products;
+            return View();
+        }
+        public ActionResult Save()
+        {
+            return View();
+        }
+    }
+}
